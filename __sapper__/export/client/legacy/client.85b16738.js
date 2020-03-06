b3730ac688ebad6bab02e4aa57fe7e2a3a99052c
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2020-03-06 11:17:30 -0600

    exported app

---
+++ __sapper__/export/client/legacy/client.85b16738.js
@@ -0,0 +1,1 @@
+import"./client.ce871341.js";
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-const {createServer} = require('https'); //RMV
-const {readFileSync} = require('fs'); //RMV
+//const {createServer} = require('https'); //RMV
+//const {readFileSync} = require('fs'); //RMV
 
 import sirv from 'sirv';
 import polka from 'polka';
@@ -9,28 +9,27 @@ import * as sapper from '@sapper/server';
 const {PORT, NODE_ENV} = process.env;
 const dev = NODE_ENV === 'development';
 
-//RMV
+/*
 const options = {
   key: readFileSync('ssl/foobar.key'),
   cert: readFileSync('ssl/foobar.crt')
 };
 console.log('server.js x: options =', options);
+*/
 
-/*
 polka() // You can also use Express
   .use(compression({threshold: 0}), sirv('static', {dev}), sapper.middleware())
   .listen(PORT, err => {
     if (err) console.log('error', err);
   });
-*/
-//RMV
+/*
 const {handler} = polka().use(
   compression({threshold: 0}),
   sirv('static', {dev}),
   sapper.middleware()
 );
 
-//RMV
 createServer(options, handler).listen(PORT, err => {
   if (err) console.log('error', err);
 });
+*/
